93b2bdcb37bb45c5430eae6bebcc016d9231a849
Alirio-Mieres <mie21001@byui.edu>
2022-01-10 18:35:22 -0400

    update

---
+++ week1/js/localstorage.js
@@ -0,0 +1,17 @@
+function loadStory(){
+  let storyName = document.querySelector("#name_input").value
+  let storyHTML = localStorage.getItem(storyName);
+  document.querySelector("#story_editor").value = storyHTML;
+}
+
+function saveStory(){
+  let storyName = document.querySelector("#name_input").value;
+  let storyHTML = document.querySelector("#story_editor").value;
+  localStorage.setItem(storyName, storyHTML);
+}
+
+function displayStory(){
+  let storyHTML = document.querySelector("#story_editor").value;
+  document.querySelector("#story_display").innerHTML = storyHTML;
+}
+
