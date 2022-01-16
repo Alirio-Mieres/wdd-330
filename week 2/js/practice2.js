3e2eba81e310dbe54ab5d0b25a564a83a485cba4
Alirio-Mieres <mie21001@byui.edu>
2022-01-15 23:42:35 -0400

    updateweek2

---
+++ week 2/js/practice2.js
@@ -0,0 +1,12 @@
+//Practice 1 Typeof
+const practiceTypeof = (type) =>{
+  const data = typeof type
+  alert(data)
+}
+
+const typeof1 = 'Hello World';
+const typeof2 = 10;
+const typeof3 = true;
+const typeof4 = {ninja: 'turtle'};
+
+
