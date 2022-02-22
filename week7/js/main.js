3ce6845930c95fad372333390bcdb063e6a8864b
Alirio-Mieres <mie21001@byui.edu>
2022-02-22 11:25:51 -0400

    update

--- week7/js/main.js
+++ week7/js/main.js
@@ -1,20 +1,20 @@
 const textButton = document.getElementById('number');
 const outputDiv = document.getElementById('output');
 
-const textURL = 'http://numbersapi.com/random';
+const textURL = 'https://numbersapi.com/random';
 
 textButton.addEventListener('click', () => {
-  fetch(textURL)
-  .then( response => {
-      outputDiv.innerHTML = 'Waiting for response...';
-  if(response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
-  .then( response => response.text() )
-  .then( text => outputDiv.innerText = text )
-  .catch( error => console.log('There was an error:', error))
+    fetch(textURL)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+    if(response.ok) {
+        return response;
+    }
+        throw Error(response.statusText);
+    })
+    .then( response => response.text() )
+    .then( text => outputDiv.innerText = text )
+    .catch( error => console.log('There was an error:', error))
 },false);
 /*const form = document.forms['todo'];
 form.addEventListener('submit', addTask, false);
